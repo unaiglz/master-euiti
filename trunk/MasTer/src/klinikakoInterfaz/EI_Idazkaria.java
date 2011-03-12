018e2175fc4a0642b08f3695c92e2f781519f601
unaiglz@gmail.com <unaiglz@gmail.com@7a4aec48-0e17-dd60-62cd-2b2e15bfc8dd>
2011-03-12 18:43:28 +0000

    Login Interfazea Programatuta

---
+++ trunk/MasTer/src/klinikakoInterfaz/EI_Idazkaria.java
@@ -0,0 +1,7 @@
+package klinikakoInterfaz;
+
+public class EI_Idazkaria {
+	public EI_Idazkaria() {
+		System.out.println("Idazkari interfazea naiz");
+	}
+}
---
+++ trunk/MasTer/src/klinikakoInterfaz/ErabiltzaileKudeatzaile.java
@@ -0,0 +1,35 @@
+package klinikakoInterfaz;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ErabiltzaileKudeatzaile {
+
+	public static void identifikazioaKonprobatu(String erab, char[] pasahitza) {
+		String pass = String.valueOf(pasahitza);
+		DBKudeatzaile dbk = DBKudeatzaile.getInstantzia();
+
+		String K1 = "SELECT * FROM Erabiltzailea WHERE izena = '" + erab
+				+ "' AND pasahitza = SHA5('" + pass + "') ";
+		// SHA5-rekin errorea ematen du
+		ResultSet rs = dbk.execSQL(K1);
+		try {
+			rs.last();
+			if (rs.getRow() != 1) {
+				loginInterfaz.errorea();
+			} else {
+				rs.beforeFirst();
+				rs.next();
+				String rol = rs.getString("rol");
+				if (rol.equalsIgnoreCase("idazkaria")) {
+					new EI_Idazkaria();
+				} else if (rol.equalsIgnoreCase("terapeuta")) {
+					new EI_Terapeuta();
+				}
+			}
+		} catch (SQLException e) {
+			// EMAITZA HUTSA
+			e.printStackTrace();
+		}
+	}
+}
