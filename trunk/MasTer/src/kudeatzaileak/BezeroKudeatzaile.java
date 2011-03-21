d89d079d3d0ce328f432bd182fe7a3a63d3129fd
lander625 <lander625@7a4aec48-0e17-dd60-62cd-2b2e15bfc8dd>
2011-03-21 03:29:55 +0000

    Idazkariaren interfazearen lehenengo bertsioa egin dut. Bezero zatia egin dut, non automatikoki datu basean dauden datuak agertzen dira. Datu basea nahi degun heinean aldatu dezakegu, emaitza ona izango da eta. Birkargatu botoiak, bezero berri bat sartzen dugunean, taula birkargatzeko balio du. Bilaketa eta ComboBox-a, edozein bezero bilatzeko balio dute.

--- trunk/MasTer/src/kudeatzaileak/BezeroKudeatzaile.java
+++ trunk/MasTer/src/kudeatzaileak/BezeroKudeatzaile.java
@@ -1,10 +1,12 @@
 package kudeatzaileak;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Date;
 
 import javax.naming.spi.DirStateFactory.Result;
+import javax.swing.table.DefaultTableModel;
 
 import datuBaseKonexioa.DBKudeatzaile;
 
@@ -46,5 +48,43 @@ public class BezeroKudeatzaile {
 			String K1 = "INSERT INTO Bezeroa VALUES('" + nan + "','"
 			+ izena + "','" + jaiotzeData + "','" + helbidea + "')";
 			dbk.execSQL(K1);
+	}
+	public void taulaBete(DefaultTableModel modelo){
+		DBKudeatzaile dbk = DBKudeatzaile.getInstantzia();
+		String K1 = "SELECT * FROM Bezeroa";
+		ResultSet rs = dbk.execSQL(K1);
+		try {
+			ResultSetMetaData metaDatos = rs.getMetaData();
+			// Se obtiene el número de columnas.
+			int numeroColumnas = metaDatos.getColumnCount();
+
+			// Se crea un array de etiquetas para rellenar
+			Object[] etiquetas = new Object[numeroColumnas];
+
+			// Se obtiene cada una de las etiquetas para cada columna
+			for (int i = 0; i < numeroColumnas; i++)
+			{
+			   // Nuevamente, para ResultSetMetaData la primera columna es la 1. 
+			   etiquetas[i] = metaDatos.getColumnLabel(i + 1); 
+			}
+			modelo.setColumnIdentifiers(etiquetas);
+			while (rs.next())
+			{
+			   // Se crea un array que será una de las filas de la tabla. 
+			   Object [] fila = new Object[numeroColumnas]; // Hay tres columnas en la tabla
+
+			   // Se rellena cada posición del array con una de las columnas de la tabla en base de datos.
+			   for (int i=0;i<numeroColumnas;i++)
+			      fila[i] = rs.getObject(i+1); // El primer indice en rs es el 1, no el cero, por eso se suma 1.
+
+			   // Se añade al modelo la fila completa.
+			   modelo.addRow(fila); 
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
