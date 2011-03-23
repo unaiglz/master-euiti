0a3dd4067c1837199e6f9ef17c7e068d1cb010ae
tails_rouge@hotmail.com <tails_rouge@hotmail.com@7a4aec48-0e17-dd60-62cd-2b2e15bfc8dd>
2011-03-23 01:11:31 +0000

    Agiria clase laguntzaile bukatu dut baina posiblea da aldatzea agirian agertzen den textua ladatzeko

--- trunk/MasTer/src/interfazeak/Agiria.java
+++ trunk/MasTer/src/interfazeak/Agiria.java
@@ -1,14 +1,40 @@
 package interfazeak;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Date;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+//String dataOrdua, String terapeutaID, String bezeroID, String terapiaMotaID
 
 public class Agiria {
+	
+	public static void main(String[] args) {
+        try {
+        	String dataOrdua=args[0];
+        	String terapeutaID=args[1];
+        	String bezeroID=args[2];
+        	String terapiaMotaID=args[2];
+        	OutputStream file = new FileOutputStream(new File("Agiria.pdf"));
 
-	public Agiria(String dataOrdua, String terapeutaID, String bezeroID, String terpiaMotaID){
-		//pdf edo text fitxategi bat sortuko du gero imprimatu ahal izateko
-		
-	}
-}
+            Document document = new Document();
+            PdfWriter.getInstance(document, file);
+            document.open();
+            document.add(new Paragraph("Gaur " + new Date().toString() + " " + bezeroID + " NANak duen bezeroak " +
+            		dataOrdua + " " + terapiaMotaID  + " terpiagatik " + terapeutaID + " NANa duen terapeutarekin egin zuen" +
+            				" terapia ordainduko du"));
+            document.add(new Paragraph("Terapiagatik ordaindu beharko du ............."));
+            document.add(new Paragraph("Idazkari Sinadura" + "                  " + "Bezero Sinadura"));	
+
+            document.close();
+            file.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }}
