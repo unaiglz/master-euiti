d503d65f3c0e60864af7fb0d21a18cad6e80410c
unaiglz@gmail.com <unaiglz@gmail.com@7a4aec48-0e17-dd60-62cd-2b2e15bfc8dd>
2011-04-01 11:04:52 +0000

    Ezer ez du Aldatu. Lander beharrezkoa duelako.

--- trunk/MasTer/src/CalendarProba.java
+++ trunk/MasTer/src/CalendarProba.java
@@ -1,3 +1,4 @@
+import java.sql.Date;
 import java.util.Calendar;
 
 import javax.swing.JFrame;
@@ -8,10 +9,15 @@ import javax.swing.JTable;
 public class CalendarProba {
 
 	private Calendar cal = Calendar.getInstance();
+	java.util.Date utilDate = new java.util.Date(); // fecha actual
+	long lnMilisegundos = utilDate.getTime();
+	private Date dat = new Date(lnMilisegundos);
 
 	public CalendarProba() {
 		JCalendar calendar = new JCalendar();
 		System.out.println(cal.getTime());
+		System.out.println(dat);
+
 	}
 
 	public static void main(String[] args) {
