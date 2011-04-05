cb354d9523582bdddc97f2a2722c1595b5b2e185
olivervicentealfonso <olivervicentealfonso@hotmail.com>
2011-04-05 17:03:00 +0000

    Terapeuta bere agenda kontsultatzeko metodoa eta interfazea eguneratu da.

--- MasTer/src/interfazeak/EI_Agenda.java
+++ MasTer/src/interfazeak/EI_Agenda.java
@@ -6,13 +6,19 @@ import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
 import java.awt.Font;
 import javax.swing.JTable;
 import javax.swing.JButton;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import kudeatzaileak.TerapeutaKudeatzaile;
 
 public class EI_Agenda {
 
@@ -21,6 +27,7 @@ public class EI_Agenda {
 	private JButton btnOnartu;
 	private DefaultTableModel modeloa;
 	private JTable tabla;
+	private JScrollPane scrollPane;
 	
 
 	/**
@@ -42,46 +49,53 @@ public class EI_Agenda {
 	/**
 	 * Create the application.
 	 */
-	public EI_Agenda(Vector<Vector<String>> informazioa) {
-		initialize(informazioa);
+	public EI_Agenda(String terapeutaNAN) {
+		initialize(terapeutaNAN);
 		//Taulan lerroak txertatzeko
-		lerroakTxertatu(informazioa);
+
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize(Vector<Vector<String>> informazioa) {
+	private void initialize(String terapeutaNAN) {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,400);
+		frame.setSize(400,452);
 		frame.setVisible(true);
 		frame.getContentPane().setLayout(null);
 		
 		lblAgenda = new JLabel("Agenda");
+		lblAgenda.setBounds(158, 11, 68, 32);
 		lblAgenda.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
-		lblAgenda.setBounds(162, 21, 68, 32);
 		frame.getContentPane().add(lblAgenda);
 		
-		//Modeloa sortu eta zutabeen izenak jartzen ditugu
-		modeloa = new DefaultTableModel();
-		modeloa.addColumn("Bezeroa");
-		modeloa.addColumn("Data");
-		modeloa.addColumn("Terapia mota");
-		
-		tabla = new JTable(modeloa);
-		tabla.setBounds(29, 78, 333, 209);
-		frame.getContentPane().add(tabla);
+		DefaultTableModel modelo = new DefaultTableModel();
+		final TableRowSorter<DefaultTableModel> ordenatzenDuena = new TableRowSorter<DefaultTableModel>(
+				modelo);
+		TerapeutaKudeatzaile tk = TerapeutaKudeatzaile.getInstantzia();
+		tk.terapeutaInterfazTaulaBete(modelo, terapeutaNAN);
 		
 		btnOnartu = new JButton("Onartu");
+		btnOnartu.setBounds(147, 351, 89, 23);
 		btnOnartu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				frame.setVisible(false);
 			}
 		});
-		btnOnartu.setBounds(151, 312, 89, 23);
 		frame.getContentPane().add(btnOnartu);		
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(19, 57, 345, 268);
+		frame.getContentPane().add(scrollPane);
+		// Instanciamos el TableRowSorter y lo añadimos al JTable
+		tabla = new JTable(modelo);
+		scrollPane.setViewportView(tabla);
+		tabla.setRowSorter(ordenatzenDuena);
+		tabla.setFillsViewportHeight(true);
+		// bakarrik selekzio bat egin ahal izateko
+		tabla.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		}
 	
 	private void lerroakTxertatu(Vector<Vector<String>> informazioa) {
